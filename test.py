58db751b555d5e55e2ba61cafb5eccde0fc45157
James Chandler <jameschandler750@gmail.com>
2022-11-22 11:05:32 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#First python program by Moneymoney122 (@chandler_hacker)
+
+import math, re
+
+print ('Hello World')
+
+def print_help():
+ print ('\tfirst python program by Moneymoney122 (@chandler_hacker)')
+
+yes = ["hello", "bye"]
+
+no = ["ok"]
+
+print(len(yes))
+
+print_help()
+
+print(yes)
+
+print ('')
+
+sus = no
+
+if sus == yes:
+
+ print ('sussy')
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y) 
+
+#end of program
